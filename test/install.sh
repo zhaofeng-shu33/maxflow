8f6144d71a7dbd1d6dbfaf1b5425f09687b368e4
zhaofeng-shu33 <616545598@qq.com>
2020-04-13 21:18:43 +0800

    add openmp compile flags

--- test/install.sh
+++ test/install.sh
@@ -3,4 +3,4 @@ echo "deb http://dl.bintray.com/zhaofeng-shu33/libgtest/ bionic main" > /etc/apt
 wget https://bintray.com/user/downloadSubjectPublicKey?username=bintray -O bintray-public.key.asc
 apt-key add bintray-public.key.asc
 apt update
-apt install --yes g++ libomp-dev libgtest1.7 cmake
+apt install --yes g++ libgtest1.7 cmake
